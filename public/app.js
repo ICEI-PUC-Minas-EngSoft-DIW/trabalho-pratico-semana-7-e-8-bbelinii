f6b8c55d0b2d689dba96d6827cda4739c265a4bf
Bernardo Belini Vale <bernardobelinivale@gmail.com>
2025-10-02 18:12:22 -0300

    Página home dinâmica

---
+++ public/app.js
@@ -0,0 +1,22 @@
+const usuarios = [
+  {
+    id: 1,
+    nome: "Pedro Henrique",
+    avatar: "img/shrek.jpg",
+    dataMembro: "10/06/2025",
+    postagem: {
+      data: "12 de setembro de 2025",
+      texto: "Fala pessoal, passando pra compartilhar uma situação chata que rolou com meu Fastback Abarth. Sempre gostei do carro, mas ultimamente venho percebendo um barulho estranho na suspensão..."
+    }
+  },
+  {
+    id: 2,
+    nome: "Maria Clara",
+    avatar: "img/barney.jpg",
+    dataMembro: "19/04/2025",
+    postagem: {
+      data: "10 de setembro de 2025",
+      texto: "Oi pessoal, vim compartilhar um problema que está me incomodando no meu Honda Civic. Nas últimas semanas, notei um barulho estranho vindo da suspensão..."
+    }
+  }
+];
